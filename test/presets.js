7af32e5b5b02257782961138d4868de34a2e45c5
Javier Cejudo <javier@javiercejudo.com>
2015-09-27 23:06:50 +1000

    test: identity preset

--- test/presets.js
+++ test/presets.js
@@ -18,7 +18,8 @@ function invert(preset) {
 
 describe('area presets', function() {
   it('should convert correctly', function() {
-    (5.045e+9).should.be.exactly(convert(5045, invert(area.squareMetre_squareKilometre)), 'squareMetre_squareKilometre')
+    (5.045e+9).should.be.exactly(convert(5.045e+9, invert(area.squareMetre_squareMetre)), 'squareMetre_squareMetre')
+      .and.exactly(convert(5045, invert(area.squareMetre_squareKilometre)), 'squareMetre_squareKilometre')
       .and.exactly(convert(504500, invert(area.squareMetre_hectare)), 'squareMetre_hectare')
       .and.exactly(convert(1947.8853898466393, invert(area.squareMetre_squareMile)), 'squareMetre_squareMile')
       .and.exactly(convert(1246646.6495018492, invert(area.squareMetre_acre)), 'squareMetre_acre')
@@ -26,7 +27,8 @@ describe('area presets', function() {
       .and.exactly(convert(54303928052.300545, invert(area.squareMetre_squareFoot)), 'squareMetre_squareFoot')
       .and.exactly(convert(7819765639531.279, invert(area.squareMetre_squareInch)), 'squareMetre_squareInch');
 
-    (0).should.be.exactly(convert(0, area.squareMetre_squareKilometre), 'squareMetre_squareKilometre')
+    (0).should.be.exactly(convert(0, area.squareMetre_squareMetre), 'squareMetre_squareMetre')
+      .and.exactly(convert(0, area.squareMetre_squareKilometre), 'squareMetre_squareKilometre')
       .and.exactly(convert(0, area.squareMetre_hectare), 'squareMetre_hectare')
       .and.exactly(convert(0, area.squareMetre_squareMile), 'squareMetre_squareMile')
       .and.exactly(convert(0, area.squareMetre_acre), 'squareMetre_acre')
